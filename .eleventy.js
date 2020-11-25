1d18b0b209e1b6d2eb1885d35154af09e5792e24
Max Kohler <max@koehler-kn.de>
2020-11-25 22:51:07 +0000

    init

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const markdownIt = require("markdown-it");
+const md = new markdownIt();
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addCollection("project", function (collectionApi) {
+    return collectionApi.getFilteredByGlob(["./projects/*.md"]);
+  });
+
+  eleventyConfig.addFilter("renderMarkdown", function (value) {
+    return md.render(value);
+  });
+
+  eleventyConfig.addPassthroughCopy("./admin");
+  eleventyConfig.addPassthroughCopy({ "./admin/config.yml": "config.yml" });
+  eleventyConfig.addPassthroughCopy("./dist");
+  eleventyConfig.addPassthroughCopy("./assets/");
+  eleventyConfig.addPassthroughCopy("*.png");
+  eleventyConfig.addPassthroughCopy("/*.xml");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
+  eleventyConfig.addWatchTarget("/dist/*.js");
+  eleventyConfig.addWatchTarget("./css/**.scss");
+  eleventyConfig.addPlugin(pluginRss);
+};
