5bbbd53d650089f91fbaffc1bc20e490c0f91983
Max Kohler <max@koehler-kn.de>
2020-12-01 10:12:05 +0000

    Set up preview slide

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
+import initSlider from "./initSlider";
+
+function initSliders() {
+  const sliders = document.querySelectorAll(".slider");
+  sliders.forEach((slider) => {
+    initSlider(slider);
+  });
+}
+
 window.addEventListener("DOMContentLoaded", () => {
-  if (navigator.appVersion.indexOf("Win") !== -1) {
+  if (navigator.appVersion.indexOf("Win") !== -1) { 
+    // UA-Sniffing to adjust for font rendering
     document.body.classList.add("is-windows");
   }
+  initSliders();
 });
---
+++ js/initSlider.js
@@ -0,0 +1,38 @@
+function initSlider(slider) {
+  let s = slider.querySelector(".slider--slides");
+  const sliderRect = s.getBoundingClientRect();
+  const statusLine = slider.querySelector(".slider-count");
+  const slides = s.querySelectorAll(".slide");
+  s.currentSlide = 0;
+
+  function setSlide(n) {
+    slides.forEach((slide, i) => {
+      slide.classList.remove("active");
+    });
+    slides[n].classList.add("active");
+  }
+
+  function xScaledToSlide(xScaled) {
+    let slide = Math.floor(xScaled / (1 / slides.length));
+    if (slide !== s.currentSlide && slide > -1 && slide < slides.length) {
+      setSlide(slide);
+	  s.currentSlide = slide;
+	  statusLine.innerText = `${s.currentSlide + 1} / ${slides.length}`;
+    }
+  }
+
+  s.addEventListener("mousemove", (e) => {
+    let xScaled = (e.clientX - sliderRect.x) / sliderRect.width;
+    xScaledToSlide(xScaled);
+  });
+
+  s.addEventListener("touchmove", (e) => {
+    e.preventDefault();
+    let xScaled = (e.touches[0].clientX - sliderRect.x) / sliderRect.width;
+    xScaledToSlide(xScaled);
+  });
+
+  setSlide(0);
+}
+
+export default initSlider;
