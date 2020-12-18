5648048d6004be726571d634b26501bb8a2676cb
Max Kohler <max@koehler-kn.de>
2020-12-18 10:52:03 +0000

    Add text replacements

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,13 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const markdownIt = require("markdown-it");
+const replacements = require("./_data/replacements.json")
 const md = new markdownIt({
   breaks: true,
 });
 
 module.exports = function (eleventyConfig) {
+  console.log(`${replacements.data.length} text replacements found.`)
+
   eleventyConfig.addCollection("directory_item", function (collectionApi) {
     return collectionApi.getFilteredByGlob(["./directory-items/*.md"]);
   });
@@ -14,14 +17,24 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addShortcode("fig", function (url, alt, className) {
-    urls = [
-      `${url.replace(/\.(jpg|jpeg|png)/gi, ".webp")}`,
-    ];
+    urls = [`${url.replace(/\.(jpg|jpeg|png)/gi, ".webp")}`];
     return `<figure class='post-figure ${className}'>
         <img src="${url}" loading="lazy" srcset="${urls[0]} 2000w" alt="${alt}">
         </figure>
         `;
   });
+  eleventyConfig.addTransform(
+    "resolveFootnotes",
+    function (content, outputPath) {
+      if (outputPath.endsWith(".html")) {
+        replacements.data.forEach(r => {
+          let re = new RegExp(r.search, "g")
+          content = content.replace(re, r.replace)
+        })
+      }
+      return content;
+    }
+  );
   eleventyConfig.addPassthroughCopy("./admin");
   eleventyConfig.addPassthroughCopy({ "./admin/config.yml": "config.yml" });
   eleventyConfig.addPassthroughCopy("./dist");
